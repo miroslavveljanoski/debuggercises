7f36976eb1aad5d72627bdebf1e6a8d688dda73c
Miroslav Veljanoski <veljanoski.miroslav@gmail.com>
2020-06-26 22:55:16 +0200

    Complete 02-primitive-types exercise

--- exercises/02-primitive-types/exercises/fill-in-the-type.js
+++ exercises/02-primitive-types/exercises/fill-in-the-type.js
@@ -3,7 +3,6 @@
 // replace the _'s to complete the challenges
 //  you know it's right when your log matches the one below it
 
-
 console.log('-- example --');
 const example = null;
 console.log('object', null);
@@ -11,26 +10,25 @@ console.log(typeof example, example);
 
 console.log('-- challenge1 --');
 const challenge1 = 'undefined';
-console.log(_, _);
+console.log('string', 'undefined');
 console.log(typeof challenge1, challenge1);
 
 console.log('-- challenge2 --');
 const challenge2 = Infinity;
-console.log(_, _);
+console.log('number', Infinity);
 console.log(typeof challenge2, challenge2);
 
 console.log('-- challenge3 --');
 const challenge3 = 4;
-console.log(_, _);
+console.log('number', 4);
 console.log(typeof challenge3, challenge3);
 
 console.log('-- challenge4 --');
 const challenge4 = '4';
-console.log(_, _);
+console.log('string', '4');
 console.log(typeof challenge4, challenge4);
 
 console.log('-- challenge5 --');
-const challenge5 = (typeof (typeof false));
-console.log(_, _);
+const challenge5 = typeof typeof false;
+console.log('string', 'string');
 console.log(typeof challenge5, challenge5);
-
