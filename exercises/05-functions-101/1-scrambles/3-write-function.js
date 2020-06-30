58a6c798a88a023f80cfe458bd297b6186c09038
Miroslav Veljanoski <veljanoski.miroslav@gmail.com>
2020-06-30 20:42:16 +0200

    Complete 05-functions exercises

--- exercises/05-functions-101/1-scrambles/3-write-function.js
+++ exercises/05-functions-101/1-scrambles/3-write-function.js
@@ -1,18 +1,17 @@
 'use strict';
 
-const scramble = () => {
-
+const scramble = (param1, param2, param3) => {
+  return `${param3}${param1}${param2}`;
 };
 
-const _1_expect = "yxz";
+const _1_expect = 'yxz';
 const _1_actual = scramble('x', 'z', 'y');
 console.assert(_1_actual === _1_expect, 'Test 1');
 
-const _2_expect = "zxy";
+const _2_expect = 'zxy';
 const _2_actual = scramble('x', 'y', 'z');
 console.assert(_2_actual === _2_expect, 'Test 2');
 
-const _3_expect = "yzx";
+const _3_expect = 'yzx';
 const _3_actual = scramble('z', 'x', 'y');
 console.assert(_3_actual === _3_expect, 'Test 3');
-
